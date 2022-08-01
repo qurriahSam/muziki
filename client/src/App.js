ca45ad7695766837276001735d95e437ae66bc46
qurriahsam <mugirethe1@gmail.com>
2022-08-01 22:59:28 +0300

    setup user state management

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { yellow } from "@mui/material/colors";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 import { Routes, Route } from "react-router-dom";
 
 const theme = createTheme({
@@ -17,12 +20,16 @@ const theme = createTheme({
 });
 
 function App() {
+  const [user, setUser] = useState({});
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
+      <Navbar user={user} />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="about" element={<SignUp />} />
+        <Route path="/" element={<LandingPage user={user} setUser={setUser} />} />
+        <Route path="signup" element={<SignUp setUser={setUser} />} />
+        <Route path="login" element={<Login setUser={setUser} />} />
+        <Route path="dashboard" element={<Dashboard user={user} />} />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
       </Routes>
     </ThemeProvider>
   );
