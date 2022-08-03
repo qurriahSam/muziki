cbeaff9f0be8cdc9a5d6b8840b0e410e27a4ec48
qurriahsam <mugirethe1@gmail.com>
2022-08-04 01:29:21 +0300

    add demo player

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Dashboard() {
-  return <div>Dashboard</div>;
+function Dashboard({ user, songUpload }) {
+  return (
+    <div>
+      <audio controls>
+        <source src="#" type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
 }
 
 export default Dashboard;
