95f4ff1b011a2a6ea61af4dee64b04ce4d47bb9c
qurriahsam <mugirethe1@gmail.com>
2022-08-03 11:37:17 +0300

    add index and create methods

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,17 @@
+class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs
+  end
+
+  def create
+    song = Song.create!(song_params)
+    render json: song, status: :created
+  end
+
+  private
+
+  def song_params
+    params.permit(:name, :artist, :user_id)
+  end
+end
