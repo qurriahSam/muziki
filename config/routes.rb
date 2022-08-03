2de0bd96d683edb9c5381e7fc3ce11422e01ab11
qurriahsam <mugirethe1@gmail.com>
2022-08-03 11:26:05 +0300

    generate routes for songs modal

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :songs, only: %i[index create]
 end
