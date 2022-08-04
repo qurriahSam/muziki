beaf21e80763dc4e515e3b89546d91f4463beeef
qurriahsam <mugirethe1@gmail.com>
2022-08-04 20:37:43 +0300

    change play icon to pause on click

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { Typography, Box, Container } from "@mui/material";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import { useState } from "react";
 
-function Song({ song: { id, name, artist } }) {
+function Song({
+  song: {
+    id,
+    name,
+    artist,
+    audio: { url },
+  },
+  setNowPlaying,
+}) {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleSongChange = () => {
+    setNowPlaying(url);
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <Box id={`${id}`} sx={{ display: "flex", pb: "2vh" }}>
       <Container sx={{ display: "flex" }}>
@@ -11,7 +28,19 @@ function Song({ song: { id, name, artist } }) {
         <Typography>{artist}</Typography>
       </Container>
       <div>
-        <PlayCircleIcon color="primary" sx={{ pr: { md: "24px", xs: "16px" } }} />
+        {isPlaying ? (
+          <PauseCircleIcon
+            color="primary"
+            sx={{ pr: { md: "24px", xs: "16px", cursor: "pointer" } }}
+            onClick={handleSongChange}
+          />
+        ) : (
+          <PlayCircleIcon
+            color="primary"
+            sx={{ pr: { md: "24px", xs: "16px", cursor: "pointer" } }}
+            onClick={handleSongChange}
+          />
+        )}
       </div>
     </Box>
   );
