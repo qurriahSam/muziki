a7d54c8a111000fa92e185d3666593d483b566c5
qurriahsam <mugirethe1@gmail.com>
2022-08-04 12:48:03 +0300

    returns song url

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,3 +1,12 @@
 class SongSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :name, :artist, :user_id, :audio
+
+  def audio
+    if object.audio.attached?
+      {
+        url: rails_blob_url(object.audio, only_path: true)
+      }
+    end
+  end
 end
