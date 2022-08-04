84109731ce88889bb5a1c621d6e11ba788f1d1ec
qurriahsam <mugirethe1@gmail.com>
2022-08-04 20:39:25 +0300

    add music player and change dashboard vh

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Box } from "@mui/material";
 import Song from "./Song";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 
 const dashboard = {
   height: "75vh",
-  marginTop: "10vh",
+  marginTop: "5vh",
 };
 
 function Dashboard({ user, songs, setSongs }) {
+  const [nowPlaying, setNowPlaying] = useState();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,7 +25,7 @@ function Dashboard({ user, songs, setSongs }) {
           const response = await fetch("/songs");
           const songs = await response.json();
           setSongs(songs);
-          console.log(songs);
+          setNowPlaying(songs[0].audio.url);
         } catch (error) {
           console.log("fetch songs error", error);
         }
@@ -29,15 +34,46 @@ function Dashboard({ user, songs, setSongs }) {
     }
   }, [user]);
 
-  const displaySongs = songs.map((song) => <Song key={song.id} song={song} />);
+  const displaySongs = songs.map((song) => (
+    <Song key={song.id} song={song} setNowPlaying={setNowPlaying} nowPlaying={nowPlaying} />
+  ));
+
+  const icons = {
+    play: <PlayCircleIcon color="secondary" fontSize="large" />,
+    pause: <PauseCircleIcon color="secondary" fontSize="large" />,
+  };
 
   return (
     <div style={dashboard}>
-      <Box maxWidth="md" sx={{ border: "1px solid red", mx: "auto" }}>
-        <Container maxWidth="md">{displaySongs}</Container>
+      <Box maxWidth="md" sx={{ border: "1px solid #D8D8D8", mx: "auto" }}>
+        <Container
+          sx={{
+            display: "flex",
+            border: "1px solid #D8D8D8",
+            textAlign: "center",
+          }}
+          disableGutters
+        >
+          <Box sx={{ width: "50%", height: "6vh", pt: "2vh", backgroundColor: "#fff176" }}>
+            Songs
+          </Box>
+          <Box sx={{ width: "50%", height: "6vh", pt: "2vh" }}>Playlist</Box>
+        </Container>
+        <Container maxWidth="md" sx={{ py: "2vh", height: "58vh", overflowY: "scroll" }}>
+          {displaySongs}
+        </Container>
       </Box>
 
-      <Container>Player</Container>
+      <Container sx={{ pt: "10px" }}>
+        <AudioPlayer
+          autoPlay
+          showSkipControls
+          showJumpControls={false}
+          src={nowPlaying}
+          customIcons={icons}
+          style={{ height: "10vh", boxShadow: "none" }}
+        />
+      </Container>
     </div>
   );
 }
