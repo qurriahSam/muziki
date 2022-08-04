0571a21b920e42dc3bfddec8db2781426bd98c60
qurriahsam <mugirethe1@gmail.com>
2022-08-04 12:47:22 +0300

    import songs from api

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Box } from "@mui/material";
+import Song from "./Song";
 
-function Dashboard({ user, songUpload }) {
+const dashboard = {
+  height: "75vh",
+  marginTop: "10vh",
+};
+
+function Dashboard({ user, songs, setSongs }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    } else {
+      const songsFetch = async () => {
+        try {
+          const response = await fetch("/songs");
+          const songs = await response.json();
+          setSongs(songs);
+          console.log(songs);
+        } catch (error) {
+          console.log("fetch songs error", error);
+        }
+      };
+      songsFetch();
+    }
+  }, [user]);
+
+  const displaySongs = songs.map((song) => <Song key={song.id} song={song} />);
+
   return (
-    <div>
-      <audio controls>
-        <source src="#" type="audio/ogg" />
-        Your browser does not support the audio element.
-      </audio>
+    <div style={dashboard}>
+      <Box maxWidth="md" sx={{ border: "1px solid red", mx: "auto" }}>
+        <Container maxWidth="md">{displaySongs}</Container>
+      </Box>
+
+      <Container>Player</Container>
     </div>
   );
 }
